8c78af39be6daf90223c469e7775448dfea61c66
bhupendra pratap <116386517+Bhupendra8896@users.noreply.github.com>
2023-07-06 01:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const container = document.querySelector(".container");
+
+  // Generate 800 squares dynamically
+  for (let i = 0; i < 800; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    container.appendChild(square);
+
+    // Add event listeners for hover and reset color after 1 second
+    square.addEventListener("mouseenter", function() {
+      this.style.backgroundColor = "coral";
+      setTimeout(() => {
+        this.style.backgroundColor = "lightgray";
+      }, 1000);
+    });
+  }
+});
+
